ff429c455d8b99de642503e7926896a5758c3a34
tanyapyndiah <tppyndiah@me.com>
2023-11-18 04:04:39 -0500

    added movie card xml

---
+++ MovieSelector/app/src/main/java/com/example/movieselector/WaitingForOthers.java
@@ -0,0 +1,14 @@
+package com.example.movieselector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WaitingForOthers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiting_for_others);
+    }
+}

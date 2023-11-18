a8b59895d60f5c7f0a1d47d03651618f9105cdcd
tanyapyndiah <tppyndiah@me.com>
2023-11-18 14:45:18 -0500

    made api work

--- MovieSelector/app/src/main/java/com/example/movieselector/MovieSearchInterface.java
+++ MovieSelector/app/src/main/java/com/example/movieselector/MovieSearchInterface.java
@@ -1,13 +1,15 @@
 package com.example.movieselector;
 
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface MovieSearchInterface {
-    @GET("/3/search/{name}")
+    @GET("/3/search/movie?api_key=5c107139744b00f06e431bab60d985a9")
     Call<Movie> getMovies(
-            @Path("name")String movieName
-    );
+             @Query("query")String movieName
+            );
 
 }
--- MovieSelector/app/src/main/java/com/example/movieselector/MovieSelector.java
+++ MovieSelector/app/src/main/java/com/example/movieselector/MovieSelector.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.SearchView;
 
 import java.util.ArrayList;

30c707e4f4adaa6e15b2519eb2b05fa379e29b2c
tanyapyndiah <tppyndiah@me.com>
2023-11-17 23:30:07 -0500

    Added UI for startpage

--- MovieSelector/app/src/main/java/com/example/movieselector/StartActivity.java
+++ MovieSelector/app/src/main/java/com/example/movieselector/StartActivity.java
@@ -5,9 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class StartActivity extends AppCompatActivity {
-    private Button create, join;
+    private ImageButton create, join;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
